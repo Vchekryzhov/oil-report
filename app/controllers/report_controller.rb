ee756116e4f777b5538fd2fa00fd646f0e4aab61
Viktor <vchekryzhov@ya.ru>
2021-12-04 08:22:15 +0300

    report

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -2,12 +2,16 @@ require 'httparty'
 class ReportController < ApplicationController
   def show
     res = HTTParty.get("https://oil-api.kovalev.team/api/issue/#{params[:issue_id]}")
+    report = HTTParty.post("https://oil-api.kovalev.team/api/issue/buildReport?issueId=#{params[:issue_id]}")
+    plant = HTTParty.get("https://oil-api.kovalev.team/api/plant/#{res['plantId']}")
+    @report = report.parsed_response
     @issue = res.parsed_response
+    @plant = plant.parsed_response
     respond_to do |format|
       format.html
       format.pdf do
-        render pdf: @issue['issueTitle'],
-              formats: :html, encoding: 'utf8'   # Excluding ".pdf" extension.
+        render pdf: @issue['issueTitle'] || 'order.pdf',
+               formats: :html, encoding: 'utf8'   # Excluding ".pdf" extension.
       end
     end
   end
