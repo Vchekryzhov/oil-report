4d8a36c718bd0b0afa74d095ed11247b38ccec2f
Viktor C <vchekryzhov@ya.ru>
2023-05-19 23:00:46 +0200

    update

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,12 +1,12 @@
 require 'httparty'
 class ReportController < ApplicationController
   def show
-    res = HTTParty.get("https://oil-api.kovalev.team/api/issue/#{params[:issue_id]}")
-    report = HTTParty.post("https://oil-api.kovalev.team/api/issue/buildReport?issueId=#{params[:issue_id]}")
-    plant = HTTParty.get("https://oil-api.kovalev.team/api/plant/#{res['plantId']}")
-    @report = report.parsed_response
-    @issue = res.parsed_response
-    @plant = plant.parsed_response
+    # res = HTTParty.get("https://oil-api.kovalev.team/api/issue/#{params[:issue_id]}")
+    # report = HTTParty.post("https://oil-api.kovalev.team/api/issue/buildReport?issueId=#{params[:issue_id]}")
+    # plant = HTTParty.get("https://oil-api.kovalev.team/api/plant/#{res['plantId']}")
+    @report = {}#report.parsed_response
+    @issue = {}#res.parsed_response
+    @plant = {}#plant.parsed_response
     respond_to do |format|
       format.html
       format.pdf do
